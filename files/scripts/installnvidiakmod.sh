acd8c76c33666158e579c09ed54d449128389a43
oomfie <f5a6bc82-27d3-4d45-aeee-26c0803e4a64@driveway.anonaddy.com>
2025-07-10 22:00:16 +0300

    update

--- files/scripts/installnvidiakmod.sh
+++ files/scripts/installnvidiakmod.sh
@@ -1,51 +1,69 @@
-#!/usr/bin/env bash
+#!/usr/bin/bash
 
-# Copyright 2025 Universal Blue
-# Copyright 2025 The Secureblue Authors
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software distributed under the License is
-# distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and limitations under the License.
+set -eoux pipefail
 
-set -oue pipefail
+# Remove Existing Kernel
+# for pkg in kernel kernel-core kernel-modules kernel-modules-core kernel-modules-extra; do
+#     rpm --erase $pkg --nodeps
+# done
 
-KERNEL_VERSION="$(rpm -q "kernel" --queryformat '%{VERSION}-%{RELEASE}.%{ARCH}')"
-RELEASE="$(rpm -E '%fedora.%_arch')"
-KERNEL_MODULE_TYPE="kernel"
-if [[ "$IMAGE_NAME" == *"open"* ]]; then
-    KERNEL_MODULE_TYPE+="-open"
+AKMODS_FLAVOR="main"
+BASE_IMAGE_NAME="silverblue"
+
+# Fetch Common AKMODS & Kernel RPMS
+skopeo copy --retry-times 3 docker://ghcr.io/ublue-os/akmods:"${AKMODS_FLAVOR}"-"$(rpm -E %fedora)" dir:/tmp/akmods
+AKMODS_TARGZ=$(jq -r '.layers[].digest' </tmp/akmods/manifest.json | cut -d : -f 2)
+tar -xvzf /tmp/akmods/"$AKMODS_TARGZ" -C /tmp/
+mv /tmp/rpms/* /tmp/akmods/
+# NOTE: kernel-rpms should auto-extract into correct location
+
+# Install Kernel
+# dnf5 -y install \
+#     /tmp/kernel-rpms/kernel-[0-9]*.rpm \
+#     /tmp/kernel-rpms/kernel-core-*.rpm \
+#     /tmp/kernel-rpms/kernel-modules-*.rpm
+
+# # TODO: Figure out why akmods cache is pulling in akmods/kernel-devel
+# dnf5 -y install \
+#     /tmp/kernel-rpms/kernel-devel-*.rpm
+
+# dnf5 versionlock add kernel kernel-devel kernel-devel-matched kernel-core kernel-modules kernel-modules-core kernel-modules-extra
+
+
+# Fetch Nvidia RPMs
+if [[ "${IMAGE_NAME}" =~ open ]]; then
+    skopeo copy --retry-times 3 docker://ghcr.io/ublue-os/akmods-nvidia-open:"${AKMODS_FLAVOR}"-"$(rpm -E %fedora)" dir:/tmp/akmods-rpms
+else
+    skopeo copy --retry-times 3 docker://ghcr.io/ublue-os/akmods-nvidia:"${AKMODS_FLAVOR}"-"$(rpm -E %fedora)" dir:/tmp/akmods-rpms
 fi
+NVIDIA_TARGZ=$(jq -r '.layers[].digest' </tmp/akmods-rpms/manifest.json | cut -d : -f 2)
+tar -xvzf /tmp/akmods-rpms/"$NVIDIA_TARGZ" -C /tmp/
+mv /tmp/rpms/* /tmp/akmods-rpms/
 
-curl -Lo /etc/yum.repos.d/negativo17-fedora-nvidia.repo https://negativo17.org/repos/fedora-nvidia.repo
-sed -i '0,/enabled=1/{s/enabled=1/enabled=1\npriority=90/}' /etc/yum.repos.d/negativo17-fedora-nvidia.repo
+# Exclude the Golang Nvidia Container Toolkit in Fedora Repo
+# Exclude for non-beta.... doesn't appear to exist for F42 yet?
+# if [[ "${UBLUE_IMAGE_TAG}" != "beta" ]]; then
+#     dnf5 config-manager setopt excludepkgs=golang-github-nvidia-container-toolkit
+# else
+    # Monkey patch right now...
+    if ! grep -q negativo17 <(rpm -qi mesa-dri-drivers); then
+        dnf5 -y swap --repo=updates-testing \
+            mesa-dri-drivers mesa-dri-drivers
+    fi
+# fi
 
-dnf install -y "kernel-devel-matched-$(rpm -q 'kernel' --queryformat '%{VERSION}')"
-dnf install -y "akmod-nvidia*.fc${RELEASE}"
+# Install Nvidia RPMs
+curl -Lo /tmp/nvidia-install.sh https://raw.githubusercontent.com/ublue-os/main/main/build_files/nvidia-install.sh
+chmod +x /tmp/nvidia-install.sh
+IMAGE_NAME="${BASE_IMAGE_NAME}" RPMFUSION_MIRROR="" /tmp/nvidia-install.sh
+rm -f /usr/share/vulkan/icd.d/nouveau_icd.*.json
+ln -sf libnvidia-ml.so.1 /usr/lib64/libnvidia-ml.so
 
-# sed -i "s/^MODULE_VARIANT=.*/MODULE_VARIANT=$KERNEL_MODULE_TYPE/" /etc/nvidia/kernel.conf
-akmods --force --kernels "${KERNEL_VERSION}" --kmod "nvidia"
+KARGS_D="/usr/lib/bootc/kargs.d"
+BLUEBUILD_NVIDIA_TOML="${KARGS_D}/00-bluebuild-nvidia-kargs.toml"
 
-# Depends on word splitting
-# shellcheck disable=SC2086
-modinfo /usr/lib/modules/${KERNEL_VERSION}/extra/nvidia/nvidia{,-drm,-modeset,-peermem,-uvm}.ko.xz > /dev/null || \
-    (cat "/var/cache/akmods/nvidia/*.failed.log" && exit 1)
-
-# View license information
-# Depends on word splitting
-# shellcheck disable=SC2086
-modinfo -l /usr/lib/modules/${KERNEL_VERSION}/extra/nvidia/nvidia{,-drm,-modeset,-peermem,-uvm}.ko.xz
-
-./signmodules.sh "nvidia"
-
-rm -f /etc/yum.repos.d/negativo17-fedora-nvidia.repo
-
-systemctl disable akmods-keygen@akmods-keygen.service
-systemctl mask akmods-keygen@akmods-keygen.service
-systemctl disable akmods-keygen.target
-systemctl mask akmods-keygen.target
+# Create kargs folder if doesn't exist
+mkdir -p "${KARGS_D}"
+tee "${BLUEBUILD_NVIDIA_TOML}" <<EOF
+kargs = ["rd.driver.blacklist=nouveau", "modprobe.blacklist=nouveau", "nvidia-drm.modeset=1", "initcall_blacklist=simpledrm_platform_driver_init"]
+EOF
